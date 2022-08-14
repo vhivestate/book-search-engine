b0a61592e28f2af6235d78d6195f356babd1a100
vhivestate <garcia.valeria001@gmail.com>
2022-08-13 23:58:09 -0600

    initial files

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function ({ req }) {
+  authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -16,7 +16,7 @@ module.exports = {
     }
 
     if (!token) {
-      return req;
+      return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -25,9 +25,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    return req;
+    // send to next endpoint
+    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
