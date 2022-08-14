c13d514f3e3b9a3e2425a08862f39e7007b129d5
vhivestate <garcia.valeria001@gmail.com>
2022-08-14 00:24:29 -0600

    deleted RESTFUL api files/code

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        username: String
+        email: String
+        _id: ID
+        savedBooks: [Book]
+    }
+    type Book {
+        authors: [String]
+        description: String
+        bookId: ID
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        user(username: String, _id: ID): User
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(
+            authors: [String],
+            description: String,
+            bookId: ID!,
+            image: String,
+            link: String,
+            title: String): Auth
+        deleteBook (bookId: String!): Auth
+    }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,43 @@
 const express = require('express');
-const path = require('path');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
