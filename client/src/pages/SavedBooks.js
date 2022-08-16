2d1b90570123701968020e034bccb222252f9da2
vhivestate <garcia.valeria001@gmail.com>
2022-08-15 19:16:01 -0600

    get user data in

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 import { useQuery, useMutation } from '@apollo/client';
@@ -9,31 +9,48 @@ import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
-
   const { loading, data } = useQuery(QUERY_ME);
-
-  console.log(data);
   const userData = data?.me || {};
-
   const handleDeleteBook = async (bookId) => {
-
-
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+  if (!Auth.loggedIn()) {
+    window.location.assign('/');
+  }
+  const [userData, setUserData] = useState({});
 
-    try { 
-      await removeBook({
-        variables: { bookId }
-      });
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
 
-      removeBookId(bookId);
-    } catch (e) {
-      console.error(e)
-    }
-  };
+  const { loading, data, refetch } = useQuery(QUERY_ME);
+  const user = data?.me;
+  const [deleteBook] = useMutation(REMOVE_BOOK);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+          return false;
+        }
+
+        const response = await getMe(token);
+
+        if (!response.ok) {
+          throw new Error('something went wrong!');
+        }
+
+        const user = await response.json();
+        setUserData(user);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUserData();
+  }, [userDataLength]);
+}
 
   // if data isn't here yet, say so
   if (loading) {
@@ -49,8 +66,8 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+          {userData.savedBooks.user
+            ? `Viewing ${userData.savedBooks.user} saved ${userData.savedBooks.user === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
