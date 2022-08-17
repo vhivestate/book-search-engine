44be6aa1c6a9d796c34a9315c886d7d71ec0b6d3
vhivestate <garcia.valeria001@gmail.com>
2022-08-16 19:33:47 -0600

    does not save front end

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -16,6 +16,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
   const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
@@ -59,26 +60,24 @@ const SearchBooks = () => {
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
       return false;
     }
-
     try {
-      const { data } = await addBook({ 
-        variables: {input: bookSave}
+      await saveBook({
+        variables: { ...bookToSave}
       });
-
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookSave.bookId]);
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
     }
   };
+
 
   return (
     <>
